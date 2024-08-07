1a3fa44dff36ac93297a74e905e161ec756ac161
JungGunWoo <j3415879@naver.com>
2024-08-07 10:39:15 +0900

    start

---
+++ src/main/java/com/cos/blog/ControllerTest.java
@@ -0,0 +1,13 @@
+package com.cos.blog;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ControllerTest {
+
+	@GetMapping("/test")
+	public String test() {
+		return "<h1>aa<h1>";
+	}
+}
